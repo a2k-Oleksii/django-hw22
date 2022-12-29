553eb21b6be3b0c6b887b3cd503b70dcadbd82c0
a2k-Oleksii <a2k-oleksii@ukr.net>
2022-12-29 23:45:13 +0200

    Create function add products to category, update category and add image to the product

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import add_product, products_details, edit_product, update_product, add_category, update_category
+
+urlpatterns = [
+    path('/add', add_product, name='add_product'),
+    path('/edit/<int:id>', edit_product, name="edit_product"),
+    path('/update/<int:id>', update_product, name="update_product"),
+    path('/category/add', add_category, name='add_category'),
+    path('/category/update/<int:id>', update_category, name='update_category'),
+    path('/<int:id>', products_details, name='products_details')
+]
---
+++ products/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import PermissionDenied
+from .models import Product, Category, CategoryProduct
+
+
+def add_product(request):
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            categories = Category.objects.all()
+            return render(request, 'products/add.html', {"categories": categories})
+        else:
+            product = Product()
+            product.title = request.POST.get('title')
+            product.description = request.POST.get('description')
+            product.user = request.user
+            product.save()
+            update_product(request, product.id)
+            return redirect('/')
+    else:
+        return redirect('/')
+
+
+def products_details(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'products/details.html', {'product': product})
+
+
+def edit_product(request, id):
+    if request.user.is_authenticated:
+        product = Product.objects.get(id=id)
+        categories = Category.objects.all()
+        product_categories = CategoryProduct.objects.filter(product_id=product.id)
+        return render(request, 'products/add.html', {
+            'product': product,
+            'product_categories': product_categories,
+            'categories': categories
+        })
+    else:
+        raise PermissionDenied
+
+
+def update_product(request, id):
+    if request.method == "GET":
+        product = Product.objects.get(id=id)
+        categories = Category.objects.all()
+        product_categories = CategoryProduct.objects.filter(product_id=product.id)
+        return render(request, 'products/add.html', {
+            'product': product,
+            'product_categories': product_categories,
+            'categories': categories
+        })
+    else:
+        if request.user.is_authenticated:
+            product = Product.objects.get(id=id)
+            product.title = request.POST.get("title")
+            product.description = request.POST.get("description")
+            product.save()
+            CategoryProduct.objects.filter(product_id=product.id).delete()
+            for category in request.POST.getlist('categories', []):
+                category_product = CategoryProduct()
+                category_product.product = product
+                category_product.category = Category.objects.get(id=int(category))
+                category_product.save()
+            return redirect("/")
+
+
+def update_category(request, id):
+    if request.method == "GET":
+        category = Category.objects.get(id=id)
+        products = Product.objects.all()
+        product_categories = CategoryProduct.objects.filter(category_id=category.id)
+        return render(request, 'products/category/add.html', {
+            'category': category,
+            'product_categories': product_categories,
+            'products': products})
+    else:
+        if request.user.is_authenticated:
+            category = Category.objects.get(id=id)
+            category.title = request.POST.get("title")
+            category.save()
+            CategoryProduct.objects.filter(category_id=category.id).delete()
+            for product in request.POST.getlist('products', []):
+                category_product = CategoryProduct()
+                category_product.category = category
+                category_product.product = Product.objects.get(id=int(product))
+                category_product.save()
+            return redirect("/")
+
+
+def add_category(request):
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            products = Product.objects.all()
+            return render(request, 'products/category/add.html', {'products': products})
+        else:
+            category = Category()
+            category.title = request.POST.get("title")
+            category.save()
+
+            update_category(request, category.id)
+            return redirect('/')
+    else:
+        return redirect('/')
